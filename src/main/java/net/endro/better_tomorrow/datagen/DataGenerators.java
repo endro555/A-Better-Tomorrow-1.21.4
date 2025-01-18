d0d787509331d16f5b0f08a98b1997a51c229b10
Endro <wyattmcgowan2008@gmail.com>
2025-01-18 13:20:52 -0700

    alalala

--- src/main/java/net/endro/better_tomorrow/datagen/DataGenerators.java
+++ src/main/java/net/endro/better_tomorrow/datagen/DataGenerators.java
@@ -6,7 +6,7 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
+
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.concurrent.CompletableFuture;
@@ -17,8 +17,6 @@ public class DataGenerators {
         public static void gatherData(GatherDataEvent event){
     DataGenerator generator = event.getGenerator();
     PackOutput packOutput = generator.getPackOutput();
-     existingFileHelper = event.GetExistingFileHelper();
-    CompletableFuture
         }
 
 }
